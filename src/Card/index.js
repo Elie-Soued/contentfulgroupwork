3565c6ac66bf2a02e1d4f7e2a804629e40c862f2
Stefan Klarmann <klarmann1@googlemail.com>
2021-01-15 16:35:54 +0100

    added Card-Component

---
+++ src/Card/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function Card() {
+  return (
+    <div className="card">
+      <div className="image">
+        <img src="https://picsum.photos/500/600" />
+      </div>
+      <div className="details">
+        <span className="favorite">
+          <i class="fa fa-star fa-2x" aria-hidden="true"></i>
+        </span>
+        <div className="center">
+          <h1>Title</h1>
+          <p>
+            Description Lorem ipsum is simple dummy text on the printing and
+            typesetting industry.
+          </p>
+          <ul>
+            <li>
+              <span>by: username</span>
+            </li>
+            <li>
+              <span>Rating: 5</span>
+            </li>
+          </ul>
+        </div>
+        <div className="plus">
+          <i class="fa fa-arrow-circle-o-right fa-3x" aria-hidden="true"></i>
+        </div>
+      </div>
+    </div>
+    //bigger Card
+  );
+}
