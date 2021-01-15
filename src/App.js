3aa04ad00eeb2bb504c440cc70adb96bf4700407
olkhon <alexander.stapel@gmail.com>
2021-01-15 18:33:19 +0100

    working get request

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import axios from "axios";
-
+import React, {useEffect, useState} from 'react';
 import AllPosts from "./views/AllPosts.js";
 import RatingPosts from "./views/RatingPosts.js";
 import LandingPage from "./views/LandingPage.js";
 
 function App() {
+
+
+
+
   return (
     <div className='App wrapper'>
-      {/*skeleton api call get
 
 
-        {axios
-        .get()
-        .then(() => {})
-        .catch(() => {})}
-      */}
+
+
+
+
+
+
 
       <Switch>
         <Route path='/allposts'>
