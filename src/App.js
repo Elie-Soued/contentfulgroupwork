2f99ebda6ee293b5b91e492568c3d893ff27dc17
Stefan Klarmann <klarmann1@googlemail.com>
2021-01-20 14:24:30 +0100

    merged elie+dev

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import AllPosts from "./views/AllPosts.js";
-import RatingPosts from "./views/RatingPosts.js";
-import LandingPage from "./views/LandingPage.js";
+import TopFivePosts from "./views/TopFivePosts.js";
+// import LandingPage from "./views/LandingPage.js";
 
 function App() {
   return (
