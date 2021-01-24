ebbf0a533b3f2cda7097a34565a95eaca1843f55
Elie <eliegebransoued@gmail.com>
2021-01-24 11:13:54 +0100

    local changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Route, NavLink, Switch } from "react-router-dom";
-
 import React from "react";
 import AllPosts from "./views/AllPosts.js";
 import TopFivePosts from "./views/TopFivePosts.js";
